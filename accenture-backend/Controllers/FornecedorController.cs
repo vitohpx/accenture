75e751207b43bfdb9ee14cb41fc963357401f6a5
vitohpx <vhpx@icomp.ufam.edu.br>
2023-07-14 14:55:35 -0400

    feat: add fornecedor controller

---
+++ accenture-backend/Controllers/FornecedorController.cs
@@ -0,0 +1,156 @@
+﻿using accenture_backend.DataContext;
+using accenture_backend.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Linq;
+using System.Text;
+
+namespace accenture_backend.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class FornecedorController : ControllerBase
+    {
+        private readonly ApiDbContext _dbContext;
+
+        public FornecedorController(ApiDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        [HttpGet]
+        public IActionResult GetFornecedores()
+        {
+
+            try
+            {
+                var fornecedores = _dbContext.Fornecedores.ToList();
+                var json = JsonConvert.SerializeObject(fornecedores);
+
+                if (fornecedores.Count == 0)
+                {
+                    return NotFound("Nenhum fornecedor encontrado.");
+                }
+                var jsonBytes = Encoding.UTF8.GetBytes(json);
+
+                return new FileContentResult(jsonBytes, "application/json");
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+
+
+        [HttpGet("{cnpjcpf}")]
+        public IActionResult GetFornecedor(string cnpjcpf)
+        {
+            var fornecedor = _dbContext.Fornecedores.FirstOrDefault(f => f.CNPJCPF == cnpjcpf);
+            if (fornecedor == null)
+            {
+                return NotFound();
+            }
+            return Ok(fornecedor);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> PostFornecedor(Fornecedor fornecedor)
+        {
+            if (_dbContext.Fornecedores.Any(f => f.CNPJCPF == fornecedor.CNPJCPF))
+            {
+                return Conflict("O CNPJ/CPF já está cadastrado.");
+            }
+
+            var cepValidationResult = await Utils.FornecedorUtils.ValidateCepAsync(fornecedor.CEP);
+            if (!cepValidationResult)
+            {
+                return BadRequest("CEP inválido.");
+            }
+
+            var empresaFornecedor = fornecedor.EmpresaCNPJ.FirstOrDefault();
+            var empresa = _dbContext.Empresas.FirstOrDefault(e => e.CNPJ == empresaFornecedor);
+            if (empresa != null && await Utils.FornecedorUtils.IsParanaEmpresaAsync(empresa) && !Utils.FornecedorUtils.IsFornecedorMaiorIdade(fornecedor.DataNascimento))
+            {
+                return BadRequest("Não é permitido cadastrar um fornecedor menor de idade para empresas do Paraná.");
+            }
+            _dbContext.Fornecedores.Add(fornecedor);
+            _dbContext.SaveChanges();
+            return CreatedAtAction(nameof(GetFornecedor), new { cnpjcpf = fornecedor.CNPJCPF }, fornecedor);
+        }
+
+        [HttpPut("{cnpjcpf}")]
+        public async Task<IActionResult> PutFornecedor(string cnpjcpf, Fornecedor fornecedor)
+        {
+            if (cnpjcpf != fornecedor.CNPJCPF)
+            {
+                return BadRequest();
+            }
+
+            var existingFornecedor = _dbContext.Fornecedores.FirstOrDefault(f => f.CNPJCPF == cnpjcpf);
+            if (existingFornecedor == null)
+            {
+                return NotFound();
+            }
+
+            if (existingFornecedor.CNPJCPF != fornecedor.CNPJCPF && _dbContext.Fornecedores.Any(f => f.CNPJCPF == fornecedor.CNPJCPF))
+            {
+                return Conflict("O CNPJ/CPF já está cadastrado.");
+            }
+
+            var cepValidationResult = await Utils.FornecedorUtils.ValidateCepAsync(fornecedor.CEP);
+            if (!cepValidationResult)
+            {
+                return BadRequest("CEP inválido.");
+            }
+
+            var empresaFornecedor = fornecedor.EmpresaCNPJ.FirstOrDefault();
+            var empresa = _dbContext.Empresas.FirstOrDefault(e => e.CNPJ == empresaFornecedor);
+            var isParanaEmpresa = await Utils.FornecedorUtils.IsParanaEmpresaAsync(empresa);
+            var isFornecedorMaiorIdade = Utils.FornecedorUtils.IsFornecedorMaiorIdade(fornecedor.DataNascimento);
+
+            if (empresa != null && isParanaEmpresa && !isFornecedorMaiorIdade)
+            {
+                return BadRequest("Não é permitido cadastrar um fornecedor menor de idade para empresas do Paraná.");
+            }
+
+            _dbContext.Entry(existingFornecedor).CurrentValues.SetValues(fornecedor);
+            _dbContext.SaveChanges();
+
+            return NoContent();
+        }
+
+        [HttpDelete("{cnpjcpf}")]
+        public IActionResult DeleteFornecedor(string cnpjcpf)
+        {
+            var fornecedor = _dbContext.Fornecedores.FirstOrDefault(f => f.CNPJCPF == cnpjcpf);
+            if (fornecedor == null)
+            {
+                return NotFound();
+            }
+
+            _dbContext.Fornecedores.Remove(fornecedor);
+            _dbContext.SaveChanges();
+
+            return NoContent();
+        }
+
+        [HttpGet("fornecedores")]
+        public IActionResult GetFornecedoresByNameAndCPF(string nome, string cpfcnpj)
+        {
+            var fornecedores = _dbContext.Fornecedores.AsQueryable();
+
+            if (!string.IsNullOrEmpty(nome))
+            {
+                fornecedores = fornecedores.Where(f => f.Nome.Contains(nome));
+            }
+
+            if (!string.IsNullOrEmpty(cpfcnpj))
+            {
+                fornecedores = fornecedores.Where(f => f.CNPJCPF.Contains(cpfcnpj));
+            }
+
+            return Ok(fornecedores.ToList());
+        }
+    }
+    
+}
---
+++ accenture-backend/Utils/FornecedorUtils.cs
@@ -0,0 +1,97 @@
+﻿using accenture_backend.Models;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Runtime.ConstrainedExecution;
+using System.Text.Json;
+
+namespace accenture_backend.Utils
+{
+    public class FornecedorUtils
+    {
+        private static readonly HttpClient _httpClient = new HttpClient();
+
+        public static async Task<bool> ValidateCepAsync(string cep)
+        {
+
+            string url = $"http://cep.la/{cep}";
+
+            try
+            {
+                HttpResponseMessage response = await _httpClient.GetAsync(url);
+                if (response.IsSuccessStatusCode)
+                {
+                    string jsonResponse = await response.Content.ReadAsStringAsync();
+                    if (!jsonResponse.Contains("Nada encontrado, tente outro pedido"))
+                    {
+                        return true;
+                    }
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+
+            return false;
+        }
+
+
+        public static async Task<bool> IsParanaEmpresaAsync(Empresa company)
+        {
+            string url = $"http://cep.la/{company.CEP}";
+
+            try
+            {
+                HttpResponseMessage response = await _httpClient.GetAsync(url);
+                if (response.IsSuccessStatusCode)
+                {
+                    string jsonResponse = await response.Content.ReadAsStringAsync();
+                    if (jsonResponse.Contains("PR"))
+                    {
+                        return true;
+                    }
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+
+            return false;
+        }
+
+        public static bool IsFornecedorMaiorIdade(DateTime? birthDate)
+        {
+            if (birthDate == null)
+                return false;
+
+            var minimumAge = 18;
+            var currentDate = DateTime.Now;
+            var birthDateValue = birthDate.Value;
+            var supplierAge = currentDate.Year - birthDateValue.Year;
+
+            if (birthDateValue > currentDate.AddYears(-supplierAge))
+            {
+                supplierAge--;
+            }
+
+            if (supplierAge >= minimumAge)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+    }
+}
